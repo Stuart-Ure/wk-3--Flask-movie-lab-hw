8c6a938568ca4bf7adf3e3af635714266e50968f
Stuart <stuartrure@gmail.com>
2023-07-21 20:03:28 +0100

    first part of code complete

--- controller/movie_controller.py
+++ controller/movie_controller.py
@@ -8,3 +8,14 @@ movies_blueprint = Blueprint("movies", __name__)
 @movies_blueprint.route('/movies')
 def index():
     return render_template('index.jinja', title= " Movie List", movies=movies)
+
+
+@movies_blueprint.route('/movies', methods= ['POST'])
+def add_movie():
+    movie_title = request.form['title']
+    movie_director = request.form['director']
+    movie_genre = request.form['genre']
+    new_movie=Movie(movie_title, movie_director, movie_genre) 
+
+    add_new_movie(new_movie)
+    return render_template('index.jinja', title= " Movie List", movies=movies)
